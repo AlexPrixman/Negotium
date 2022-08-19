4b64d9bc9fe4cbdef748d091a5bcdf77587029b3
Alex Prixman <alexprixman@gmail.com>
2022-08-19 08:48:54 +0800

    Adding options select

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import './App.css';
-import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Container, Form, Button } from 'react-bootstrap';
+import Select from 'react-select'
 
 function App() {
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+            <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto" pullRight>
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.1">List</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Search</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
+              <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-primary">Search</Button>
+            </Form> 
             </Navbar.Collapse>
           </Container>
         </Navbar>
+        <Select isClearable options={options} />
     </div>
   );
 }
